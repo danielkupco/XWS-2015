e3e253d78732ff0618b67d7acb6a3a11655058d2
danielkupco <kupcodandroid@gmail.com>
2015-09-19 14:03:36 +0200

    Osnova za rest

---
+++ XWS-REST/src/xws/rest/entities/Identifiable.java
@@ -0,0 +1,13 @@
+package xws.rest.entities;
+
+/**
+ * Olaksava implementaciju generickog data access object bean-a. 
+ *
+ */
+public abstract class Identifiable {
+
+	public abstract Long getId();
+	
+	public abstract void setId(Long value);
+	
+}
